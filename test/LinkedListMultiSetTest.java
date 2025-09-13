3a74f800e6f2c590b73931d99f0fcd73a010e695
Jonathan Calver <calver@cs.toronto.edu>
2025-09-13 09:49:49 -0400

    fixing class name in test

--- test/LinkedListMultiSetTest.java
+++ test/LinkedListMultiSetTest.java
@@ -6,27 +6,27 @@ public class LinkedListMultiSetTest {
 
     @Test
     public void testIsEmptyTrue() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         assertTrue(b.isEmpty());
     }
 
     @Test
     public void testIsEmptyFalse() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         b.add(5);
         assertFalse(b.isEmpty());
     }
 
     @Test
     public void testInsertEmpty() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         b.add(4);
         assertTrue(b.contains(4));
     }
 
     @Test
     public void testInsertNonEmpty() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         b.add(10);
         b.add(4);
         assertTrue(b.contains(4));
@@ -34,7 +34,7 @@ public class LinkedListMultiSetTest {
 
     @Test
     public void testRemoveRoot() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         b.add(10);
         b.remove(10);
         assertFalse(b.contains(10));
@@ -43,7 +43,7 @@ public class LinkedListMultiSetTest {
 
     @Test
     public void testRemoveNonRoot() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         b.add(10);
         b.add(4);
         b.remove(4);
@@ -51,20 +51,20 @@ public class LinkedListMultiSetTest {
     }
     @Test
     public void testSizeEmpty() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         assertEquals(0, b.size());
     }
 
     @Test
     public void testSizeOne() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         b.add(10);
         assertEquals(1, b.size());
     }
 
     @Test
     public void testSizeNonEmpty() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         b.add(10);
         b.add(4);
         b.add(12);
@@ -74,20 +74,20 @@ public class LinkedListMultiSetTest {
 
     @Test
     public void testCountEmpty() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         assertEquals(0, b.count(10));
     }
 
     @Test
     public void testCountOne() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         b.add(11);
         assertEquals(1, b.count(11));
     }
 
     @Test
     public void testCountNonEmpty() {
-        LinkedListMultiSetSol b = new LinkedListMultiSetSol();
+        LinkedListMultiSet b = new LinkedListMultiSet();
         b.add(10);
         b.add(10);
         b.add(12);
