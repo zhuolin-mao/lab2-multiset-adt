73273d4032b2aeb977da38ca40bd696ee3273143
Lenovo <franklin.mao@mail.utoronto.ca>
2025-10-03 13:46:54 -0400

    `Implement BST insert/delete 3rd`

--- src/BSTMultiSet.java
+++ src/BSTMultiSet.java
@@ -11,6 +11,7 @@ public class BSTMultiSet extends MultiSet {
      */
     @Override
     void add(int item) {
+        bst.insert(item);
     }
 
     /**
@@ -21,6 +22,7 @@ public class BSTMultiSet extends MultiSet {
      */
     @Override
     void remove(int item) {
+        bst.delete(item);
     }
 
     /**
@@ -31,7 +33,7 @@ public class BSTMultiSet extends MultiSet {
      */
     @Override
     boolean contains(int item) {
-        return false;
+        return bst.contains(item);
     }
 
     /**
@@ -39,7 +41,7 @@ public class BSTMultiSet extends MultiSet {
      */
     @Override
     boolean isEmpty() {
-        return false;
+        return bst.isEmpty();
     }
 
     /**
@@ -50,7 +52,7 @@ public class BSTMultiSet extends MultiSet {
      */
     @Override
     int count(int item) {
-        return -1;
+        return bst.count(item);
     }
 
     /**
@@ -58,6 +60,6 @@ public class BSTMultiSet extends MultiSet {
      */
     @Override
     int size() {
-        return -1;
+        return bst.getSize();
     }
 }
